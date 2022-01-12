7d903868ae05a4a6798f954c14b9cca82c227ef9
Denys <dentor2005@gmail.com>
2022-01-12 21:36:21 +0100

    base project structure

--- app/src/main/java/com/entin/vsetury/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/entin/vsetury/presentation/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.entin.vsetury
+package com.entin.vsetury.presentation.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.entin.vsetury.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
